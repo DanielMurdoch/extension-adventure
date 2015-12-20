65b875bdb0c9435936a3e11504875c9e16fb4d52
Daniel Murdoch <danieltheunicorn@gmail.com>
2015-12-19 23:43:02 -0500

    Messaging working as intended. Injected script now has access to the RMP page data. MVP should be smooth from here on out.

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,19 +3,24 @@
 // var settings = new Store("settings", {
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
+
+function rmpGetRequest(request, sender, sendResponse) {
+	$.get(request.url, function(data){
+		sendResponse({data: data})
+	}).fail(function(){
+		sendResponse({data: "Fail"})
+	})
+	//sendResponse({farewell: request.url});
+}
+
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
+    if (request.tag == "fetch_rmp_data") {
+      	rmpGetRequest(request, sender, sendResponse)
+      	return true;
+    } else {
+    	chrome.pageAction.show(sender.tab.id);
+    	sendResponse({data: "for real"});
+    }
   });
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,19 +8,10 @@ chrome.extension.sendMessage({}, function(response) {
 			profURL = 'http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=university+of+ottawa&queryoption=HEADER&query=' + profName.replace(' ', '+') + '&facetSearch=true'
 
 			this.innerHTML = "<a href=''>" +  profName +  "</a>"
-			
-			chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  				console.log(response.farewell);
+
+			chrome.runtime.sendMessage({tag: "fetch_rmp_data", url: profURL}, function(response) {
+  				console.log(response);
 			});
-
-
-
-
-
-			
-
-
-
 
 			//console.log(profName)
 			//console.log(profURL)
