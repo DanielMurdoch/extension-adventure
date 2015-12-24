f180f828ae32ac2107424d69323df00b8cb8d277
Daniel Murdoch <danieltheunicorn@gmail.com>
2015-12-23 22:23:05 -0500

    Reformat the jquery page editing to remove loading jutter. Complete line by line commenting on the injected script.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,32 +10,40 @@
 **/
 const UOTTAWA_RMP_PREFIX = 'http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=university+of+ottawa&queryoption=HEADER&query='
 const UOTTAWA_RMP_SUFFIX = '&facetSearch=true'
+const BASE_STRUCTURE = '<td><div id="rmp_overall">Overall:&nbsp</div> <div id="rmp_average">Avg.Grade:&nbsp</div> <div id="rmp_help">Help:&nbsp</div> <div id="rmp_clarity">Clarity:&nbsp</div> <div id="rmp_easiness">Easiness:&nbsp</div></td>'
+
 
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === 'complete') {
 		clearInterval(readyStateCheckInterval);
-
+		//Insert new TH after any instances of Professor TH.
 		$('<th>RMP Stats</th>').insertAfter($('[id^=hdr_professor]'))
-
+		//Extend footers of sections in the table, just to make things pretty.
 		$('.footer').each(function(index, element) {
 			$($(element).children()[0]).attr('colspan', 6)
 		})
-
+		//Iterate over each Professor element. This includes both / all tables on the page.
 		$('.Professor').each(function(index, element) {
+			//Get the prof's name, massage.
 			var profName = this.innerHTML
 			var profURL =  UOTTAWA_RMP_PREFIX + profName.replace(' ', '+') + UOTTAWA_RMP_SUFFIX
+			//Inserts the basic structure of the RMP column before fetching anything, prevents unsightly load times and page jutter.
+			$(BASE_STRUCTURE).insertAfter($(element)) 
+			//Call the event/background script to do the gets using a message listener.
 			chrome.runtime.sendMessage({tag: 'fetch_rmp_data', url: profURL, name: profName}, function(response) {	
+  				//If the ajax calls don't fail, then the response will be non-null.
   				if(response.link) {
-  					$(element).html('<a href=' + response.link + '>' + response.name + '</a>')
-
-  					var listContent = "Overall:&nbsp" + response.overallGrade + " Avg.Grade:&nbsp" + response.averageGrade + " Help:&nbsp" + response.helpfullness + " Clarity:&nbsp" + response.clarity + " Easiness:&nbsp" + response.easiness
-
-  					$('<td>' + listContent + '</td>').insertAfter($(element)) 
+  					//Edits the prof column, turning the name into a link to their RMP page.
+  					$(element).html('<a target="_blank" href=' + response.link + '>' + response.name + '</a>')
+  					//Fills the data in the new RMP column.
+  					$(element).next().find('#rmp_overall').append(response.overallGrade)
+  					$(element).next().find('#rmp_average').append(response.averageGrade)
+  					$(element).next().find('#rmp_help').append(response.helpfullness)
+  					$(element).next().find('#rmp_clarity').append(response.clarity)
+  					$(element).next().find('#rmp_easiness').append(response.easiness)
   				}
 			});
-
-			
 		})
 	}
 	}, 10);
